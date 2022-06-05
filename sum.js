dde2f6b364c16aeb5679f6027103815f115ec024
moha-sak <maxamedyare236@gmail.com>
2022-06-05 09:42:11 +0300

    feature 1.2

--- sum.js
+++ sum.js
@@ -1,3 +1,4 @@
+//this is sum function that we do a sum with array
 function sum(...values)
 {
     let sum = 0;
@@ -8,4 +9,4 @@ function sum(...values)
 return sum;
 }
 
-console.log(sum(3,5,2,5,3,2,5));
+console.lo(sum(3,5,2,5,3,2,5));
